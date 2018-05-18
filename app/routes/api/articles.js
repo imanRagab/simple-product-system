bcf26e4fbf31fd30fc62bc5f2dbb5141ef2d6d65
Iman <iman.mansour94@gmail.com>
2018-05-18 23:27:45 +0200

    update article route

--- app/routes/api/articles.js
+++ app/routes/api/articles.js
@@ -5,8 +5,11 @@ var articlesController = require('../../controllers/api/articlesController')
 /* GET articles listing. */
 router.get('/', articlesController.listArticles);
 
-/* POST articles listing. */
+/* POST article create. */
 router.post('/', articlesController.createArticle);
+
+/* PUT article update. */
+router.put('/:id', articlesController.updateArticle);
 
 module.exports = router;
 
