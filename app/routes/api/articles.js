c636a2aaae805a624b6cc6586be986bb9c9ecf51
Iman <iman.mansour94@gmail.com>
2018-05-18 22:43:06 +0200

    add create article route

--- app/routes/api/articles.js
+++ app/routes/api/articles.js
@@ -5,6 +5,9 @@ var articlesController = require('../../controllers/api/articlesController')
 /* GET articles listing. */
 router.get('/', articlesController.listArticles);
 
+/* POST articles listing. */
+router.post('/', articlesController.createArticle);
+
 module.exports = router;
 
 
