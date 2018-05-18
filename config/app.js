d05e1b564331ea587d79291e59973bf3d1855e82
Iman <iman.mansour94@gmail.com>
2018-05-18 20:55:50 +0200

    app connect to db

--- config/app.js
+++ config/app.js
@@ -3,6 +3,10 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var db = require('./db');
+
+// connect to database
+var mongoose = db.connect();
 
 var app = express();
 
@@ -10,7 +14,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-
+// add app middlewares
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
--- config/db.js
+++ config/db.js
@@ -17,3 +17,4 @@ module.exports.disconnect = function (){
     mongoose.disconnect();
 }
 
+
