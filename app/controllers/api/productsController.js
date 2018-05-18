893336072ef3572eac9605db428d5cf4baadac80
Iman <iman.mansour94@gmail.com>
2018-05-19 00:13:03 +0200

    create product function

--- app/controllers/api/productsController.js
+++ app/controllers/api/productsController.js
@@ -10,3 +10,18 @@ exports.listProducts = function(req, res) {
       res.json(products);
     });
 }
+
+// create product
+
+exports.createProduct = function(req, res) {
+
+  Product.create({
+    no : req.body.no,
+    name : req.body.name,
+    article : req.body.article
+  }, 
+  function (err, product) {
+      if (err) return res.status(500).send("There was a problem adding the information to the database.");
+      res.status(200).send(product);
+  });
+}
