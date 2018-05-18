a1d550576b91a6f35f96638f8035ad3f80aa499c
Iman <iman.mansour94@gmail.com>
2018-05-18 20:21:35 +0200

    db config - add connect function

--- config/db.js
+++ config/db.js
@@ -4,3 +4,12 @@ var mongoose = require('mongoose');
 const HOST = process.env.DATABASE_HOST;
 const DB_NAME = process.env.DATABASE_NAME;
 const CONNECTION_STRING = `mongodb://${HOST}/${DB_NAME}`;
+
+// db connect
+module.exports.connect = function(){
+        return mongoose.connect(CONNECTION_STRING).catch(function (error) {
+        console.log('Unable to connect to the mongodb instance. Error: ', error);
+    });
+}
+
+
