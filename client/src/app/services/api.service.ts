6bc0e6d9cc27cf738071b27d2861442dff31b0b4
Iman <iman.mansour94@gmail.com>
2018-05-19 18:21:34 +0200

    add get function - api service

--- client/src/app/services/api.service.ts
+++ client/src/app/services/api.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import {Observable} from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { HttpHeaders } from '@angular/common/http';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() { }
+  apiUrl = 'http://localhost:3000';
+
+  constructor(private http: HttpClient) { }
+
+  // get data from api
+
+  get(endPoint: string): Observable<any> {
+
+    return this.http.get(this.apiUrl + endPoint);
+  }
 }
