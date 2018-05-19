ed094f31f95c026a104db5296881cee42797fcfd
Iman <iman.mansour94@gmail.com>
2018-05-19 22:59:57 +0200

    create article form

--- client/src/app/article/article.module.ts
+++ client/src/app/article/article.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { ListComponent } from './list/list.component';
 import { RouterModule } from '@angular/router';
 import { CreateComponent } from './create/create.component';
@@ -7,6 +8,7 @@ import { CreateComponent } from './create/create.component';
 @NgModule({
   imports: [
     CommonModule,
+    FormsModule,
     RouterModule.forRoot([
       { path: 'articles', component: ListComponent},
       { path: 'articles/create', component: CreateComponent},
