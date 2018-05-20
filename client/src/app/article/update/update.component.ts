d17f8af05ba7b33b29a0f94326878986bbe69826
Iman <iman.mansour94@gmail.com>
2018-05-20 02:25:52 +0200

    get article function - article update component

--- client/src/app/article/update/update.component.ts
+++ client/src/app/article/update/update.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ApiService } from '../../services/api.service';
 
 @Component({
   selector: 'app-update',
@@ -9,14 +10,29 @@ import { ActivatedRoute } from '@angular/router';
 export class UpdateComponent implements OnInit {
 
   id: string;
+  articleForm: object;
   constructor(
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private api: ApiService
   ) {
     this.id = this.route.snapshot.params.id;
-    console.log(this.id);
+    this.getArticle();
   }
-
   ngOnInit() {
   }
 
+  // get article data
+
+  getArticle(): void {
+
+    this.api.get(`/api/articles/${this.id}`).subscribe(
+      res => {
+        this.articleForm = res;
+      },
+      err => {
+        console.log(err);
+      }
+    );
+
+  }
 }
